66436311bdac09307c711a50bb110fca82b015d9
Jackie <106541054+damnjackie@users.noreply.github.com>
2022-06-01 09:27:04 +0530

    Update streamlit_app.py
    added header and texts

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,2 +1,7 @@
 import streamlit
 streamlit.title("I'M GOING TO GO OUT, MEET SOME BOYS, AND CRUSH THEIR HEARTS ONE BY ONE")
+
+streamlit.header('List')
+streamlit.text('Hyde')
+streamlit.text('Kelso')
+streamlit.text('Faiz')
